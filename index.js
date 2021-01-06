e3ebb035c228f5226fb25390d869a95bf17d62e8
cameronevans <cam.abevans@gmail.com>
2021-01-06 18:32:09 -0400

    added markdown-it-task-lists options to config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function activate(context) {
     return {
         extendMarkdownIt(md) {
-            return md.use(require('markdown-it-task-lists'));
+            return md.use(require('markdown-it-task-lists'), require('vscode').workspace.getConfiguration().get('vscode-markdown-checkboxes.itTaskListsOptions'));
         }
     };
 }
