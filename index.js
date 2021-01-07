7192277d753f3226e93798009c556a5b530a4c00
cameronevans <cam.abevans@gmail.com>
2021-01-07 11:17:01 -0400

    pr feedback - individual setting for each markdown-it-task-lists option

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const { workspace } = require('vscode');
 function activate(context) {
     return {
         extendMarkdownIt(md) {
-            return md.use(require('markdown-it-task-lists'), workspace.getConfiguration().get('vscode-markdown-checkboxes.itTaskListsOptions'));
+            return md.use(require('markdown-it-task-lists'), {
+                enabled:  workspace.getConfiguration().get('vscode-markdown-checkboxes.enable'),
+                label:  workspace.getConfiguration().get('vscode-markdown-checkboxes.label'),
+                labelAfter:  workspace.getConfiguration().get('vscode-markdown-checkboxes.labelAfter')
+            });
         }
     };
 }
