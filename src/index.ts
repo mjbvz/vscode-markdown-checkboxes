d7766e92ff30beeee8736e16216f595b33a56caa
Matt Bierner <matb@microsoft.com>
2021-09-08 10:35:47 -0700

    Use * import

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,17 @@
 import { workspace } from 'vscode';
-import taskList from 'markdown-it-task-lists';
+import * as taskList from 'markdown-it-task-lists';
 import type MarkdownIt from 'markdown-it';
 
 export function activate() {
     return { 
         extendMarkdownIt(md: MarkdownIt) {
             return md.use(() => {
-                console.log('xx')
                 const config = workspace.getConfiguration('markdown-checkboxes');
-                const x = md.use(taskList, {
+                return md.use(taskList, {
                     enabled: config.get('enable'),
                     label: config.get('label'),
                     labelAfter: config.get('labelAfter')
                 });
-                const y = x.use(taskList, {
-                    enabled: config.get('enable'),
-                    label: config.get('label'),
-                    labelAfter: config.get('labelAfter')
-                });
-                return md;
             });
         } 
     };
